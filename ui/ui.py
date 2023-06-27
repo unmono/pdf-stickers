c23680065703f9cf008d46d9b36d039f698815c9
Roma <you@example.com>
2023-06-27 22:08:37 +0200

    Refactor ui to class to be able to manage state.

--- ui/ui.py
+++ ui/ui.py
@@ -1,141 +1,108 @@
 import os
-from typing import Iterable
+from typing import Iterable, Callable
 from pathlib import Path
+from dataclasses import dataclass
 
 import tkinter as tk
 from tkinter import ttk, filedialog
 
 from app.__main__ import compose_stickers
 
-root = tk.Tk()
-root.title("NP stickers")
 
-# Variables:
-file_list: list[Path] = []
-cvar_radios = tk.IntVar()
-
-# Frames:
-frm_radios = tk.Frame(
-    master=root,
-    width=500,
-    pady=5,
-)
-frm_buttons = tk.Frame(
-    master=root,
-    width=500,
-    padx=10,
-    pady=5,
-)
-frm_file_list = tk.Frame(
-    master=root,
-    width=500,
-    height=100,
-    padx=10,
-    pady=5,
-    bd=1,
-    relief=tk.SUNKEN,
-)
-
-frm_radios.pack(fill=tk.X)
-frm_buttons.pack(fill=tk.X)
-frm_file_list.pack(fill=tk.BOTH, expand=True)
-
-
-# Radios:
-rbtn1 = tk.Radiobutton(
-    master=frm_radios,
-    variable=cvar_radios,
-    value=0,
-    text='Select directory',
-)
-rbtn2 = tk.Radiobutton(
-    master=frm_radios,
-    variable=cvar_radios,
-    value=1,
-    text='Select individual files',
-)
-rbtn1.grid(column=0, row=0, sticky='nw')
-rbtn2.grid(column=0, row=1, sticky='nw')
-
-
-def browse_files() -> Iterable:
+def browse_files() -> Iterable[Path]:
     return tk.filedialog.askopenfilenames(
         defaultextension='.pdf',
         filetypes=[('PDF', ('*.pdf', '*.PDF')), ]
     )
 
 
-def browse_directory() -> Iterable:
+def browse_directory() -> Iterable[Path]:
     dir_name = tk.filedialog.askdirectory()
     return [Path(dir_name) / f for f in os.listdir(dir_name) if f.endswith(('.pdf', '.PDF', ))] \
         if dir_name \
         else []
 
 
-browse_functions = [
+class StickersUI:
+    def __init__(self, browse_functions: list[Callable[[], Iterable[Path]]]):
+        self.root = tk.Tk()
+        self.root.title = "NP stickers"
+
+        # State variables
+        self._browse_mode = tk.IntVar()
+        self._file_list = []
+        self.browse_functions = browse_functions
+
+        # Frames:
+        self.frm_radios = tk.Frame(master=self.root, width=500, pady=5)
+        self.frm_buttons = tk.Frame(master=self.root, width=500, padx=10, pady=5)
+        self.frm_file_list = tk.Frame(master=self.root, width=500, height=100, padx=10, pady=5, bd=1, relief=tk.SUNKEN)
+
+        self.frm_radios.pack(fill=tk.X)
+        self.frm_buttons.pack(fill=tk.X)
+        self.frm_file_list.pack(fill=tk.BOTH, expand=True)
+
+        # Radios:
+        self.rbtn1 = tk.Radiobutton(master=self.frm_radios,
+                                    variable=self._browse_mode,
+                                    value=0,
+                                    text='Select directory')
+        self.rbtn2 = tk.Radiobutton(master=self.frm_radios,
+                                    variable=self._browse_mode,
+                                    value=1,
+                                    text='Select individual files')
+        self.rbtn1.grid(column=0, row=0, sticky='nw')
+        self.rbtn2.grid(column=0, row=1, sticky='nw')
+
+        # Buttons
+        self.btn_browse = tk.Button(master=self.frm_buttons, text='Browse', command=self.browse)
+        self.btn_clear = tk.Button(master=self.frm_buttons, text='Clear', command=self.clear, state=tk.DISABLED)
+        self.btn_save = tk.Button(master=self.frm_buttons, text='Save', command=self.save, state=tk.DISABLED)
+        self.btn_browse.pack(side=tk.LEFT)
+        self.btn_clear.pack(side=tk.LEFT, padx=4)
+        self.btn_save.pack(side=tk.RIGHT)
+
+        # Run mainloop
+        self.root.mainloop()
+
+    @property
+    def browse_mode(self) -> int:
+        return self._browse_mode.get()
+
+    @property
+    def file_list(self) -> list[Path]:
+        return self._file_list
+
+    @file_list.setter
+    def file_list(self, value):
+        self._file_list = value
+        for w in self.frm_file_list.winfo_children():
+            w.destroy()
+        if self._file_list:
+            self.btn_clear.config(state=tk.ACTIVE)
+            self.btn_save.config(state=tk.ACTIVE)
+            for i, f in enumerate(self._file_list):
+                tk.Label(master=self.frm_file_list, text=str(f.resolve())).grid(column=0, row=i, sticky='nw')
+        else:
+            self.btn_clear.config(state=tk.DISABLED)
+            self.btn_save.config(state=tk.DISABLED)
+
+    def browse(self):
+        chosen_files = self.browse_functions[self.browse_mode]()
+        self.file_list += [Path(f) for f in chosen_files]
+
+    def clear(self):
+        self.file_list = []
+
+    def save(self):
+        file_to_save = tk.filedialog.asksaveasfilename(
+            defaultextension='.pdf',
+            filetypes=[('PDF', ('*.pdf', '*.PDF')), ]
+        )
+        compose_stickers(self.file_list, file_to_save)
+
+
+stickers = StickersUI([
     browse_directory,
     browse_files,
-]
-
-
-def list_files(fl: list[Path]):
-    for w in frm_file_list.winfo_children():
-        w.destroy()
-
-    for i, f in enumerate(fl):
-        tk.Label(
-            master=frm_file_list,
-            text=str(f.resolve()),
-        ).grid(column=0, row=i, sticky='nw')
-
-
-def browse():
-    global file_list
-
-    browsing_mode = cvar_radios.get()
-    chosen_files = browse_functions[browsing_mode]()
-
-    file_list += [Path(f) for f in chosen_files]
-
-    if file_list:
-        list_files(file_list)
-
-
-def clear():
-    global file_list
-
-    file_list = []
-    list_files(file_list)
-
-
-def save():
-    global file_list
-
-    file_to_save = tk.filedialog.asksaveasfilename(
-        defaultextension='.pdf',
-        filetypes=[('PDF', ('*.pdf', '*.PDF')), ]
-    )
-    compose_stickers(file_list, file_to_save)
-
-
-btn_browse = tk.Button(
-    master=frm_buttons,
-    text='Browse',
-    command=browse,
-)
-btn_clear = tk.Button(
-    master=frm_buttons,
-    text='Clear',
-    command=clear,
-)
-btn_save = tk.Button(
-    master=frm_buttons,
-    text='Save',
-    command=save,
-)
-
-btn_browse.pack(side=tk.LEFT)
-btn_clear.pack(side=tk.LEFT, padx=4)
-btn_save.pack(side=tk.RIGHT)
-
-root.mainloop()
+])
