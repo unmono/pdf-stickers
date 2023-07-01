cbdb5aa1a80df185c5cbd6c072ff4f04833053b7
Roma <you@example.com>
2023-07-01 18:59:28 +0200

    Add an option to chose paper format and layout options. Now they can be specified by cli using options: -w for number of stickers in page widh -h for number of stickers in page height -s for paper size in format 'a4', 'A5'

--- app/__main__.py
+++ app/__main__.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pathlib
 from typing import Iterable
+from functools import partial
 
 from pypdf import (
     PdfReader,
@@ -23,6 +24,70 @@ class UnporcessableArgumentsError(Exception):
         return f'These arguments cannot be processed:\n{args_repr}'
 
 
+def validate_grid_value(*args) -> list[tuple[str, str]]:
+    errors = []
+    for value in args:
+        if value < 1 or value > 40:
+            errors.append((value, 'Should be an integer from 1 to 40.'))
+
+    return errors
+
+
+def sticker_stacker(
+        stickers: list[PageObject],
+        paper_format: str = 'A4',
+        # Initial goal is to fit 6(2x3 grid) stickers on A4 page, so the default values are 2 and 3
+        stickers_in_width: int = 2,
+        stickers_in_height: int = 3,
+) -> PdfWriter:
+    """
+    Creates a PdfWriter object with all stickers placed on A4 pages.
+
+    :param paper_format: paper format to use (e.g. A4, A3 etc.)
+    :param stickers_in_width: how many stickers do you want to place in a single row on a page(stickers in width)
+    :param stickers_in_height: how many rows of stickers should be on a page(stickers in height)
+    :param stickers: list of PageObject representing each sticker
+    :return: PdfWriter object ready to write in file
+    """
+
+    errors = []
+    try:
+        page_size = getattr(PaperSize, paper_format.upper())
+    except AttributeError:
+        valid_formats = [f for f in dir(PaperSize) if not f.startswith('__')]
+        errors.append((paper_format, f'It is not a valid paper format. Please chose from: {", ".join(valid_formats)}'))
+
+    errors += validate_grid_value(stickers_in_width, stickers_in_height)
+    if errors:
+        raise UnporcessableArgumentsError(errors)
+
+    stickers_on_page = stickers_in_width * stickers_in_height
+    sticker_widht = page_size.width / stickers_in_width
+    sticker_height = page_size.height / stickers_in_height
+
+    writer = PdfWriter()
+
+    destpage: PageObject | None = None
+    for i, s in enumerate(stickers):
+        if i % stickers_on_page == 0:
+            destpage = writer.add_blank_page(
+                width=page_size.width,
+                height=page_size.height,
+            )
+        s.scale_to(width=sticker_widht, height=sticker_height)
+
+        x = i % stickers_in_width
+        y = (i % stickers_on_page) // stickers_in_width
+        destpage.merge_transformed_page(
+            s,
+            Transformation().translate(
+                x * sticker_widht,
+                page_size.height - (y + 1) * sticker_height,  # origin is left bottom corner
+            ),
+        )
+    return writer
+
+
 def sticker_list(files_list: list[PdfReader]) -> list[PageObject]:
     """
     Make list of individual PageObject objects representing each sticker
@@ -34,48 +99,6 @@ def sticker_list(files_list: list[PdfReader]) -> list[PageObject]:
     for f in files_list:
         stickers += f.pages
     return stickers
-
-
-def sticker_stacker(stickers: list[PageObject]) -> PdfWriter:
-    """
-    Creates a PdfWriter object with all stickers placed on A4 pages.
-
-    :param stickers: list of PageObject representing each sticker
-    :return: PdfWriter object ready to write in file
-    """
-
-    # Initial goal is to fit 6(2x3 grid) stickers on A4 page
-    #
-    # Need to think how to bind this numbers together when the grid will be
-    # a subject to change
-    sticker_widht = PaperSize.A4.width / 2
-    sticker_height = PaperSize.A4.height / 3
-
-    writer = PdfWriter()
-
-    destpage: PageObject | None = None
-    for i, s in enumerate(stickers):
-        # Need to think how to bind this numbers together when the grid will be
-        # a subject to chage
-        if i % 6 == 0:
-            destpage = writer.add_blank_page(
-                width=PaperSize.A4.width,
-                height=PaperSize.A4.height,
-            )
-        s.scale_to(width=sticker_widht, height=sticker_height)
-
-        # Need to think how to bind this numbers together when the grid will be
-        # a subject to chage
-        x = i % 2
-        y = (i % 6) // 2
-        destpage.merge_transformed_page(
-            s,
-            Transformation().translate(
-                x * sticker_widht,
-                PaperSize.A4.height - (y + 1) * sticker_height,  # origin is left bottom corner
-            ),
-        )
-    return writer
 
 
 def process_paths(files_list: list[str | os.PathLike]) -> list[PdfReader]:
@@ -114,7 +137,8 @@ def process_paths(files_list: list[str | os.PathLike]) -> list[PdfReader]:
 
 def compose_stickers(
     files_list: list[str | os.PathLike],
-    file_to_write: str | os.PathLike = 'stickers.pdf'
+    file_to_write: str | os.PathLike = 'stickers.pdf',
+    **kwargs
 ) -> None:
     """
     Make .pdf file with all pages from listed files as stickers placed on A4 page.
@@ -123,7 +147,7 @@ def compose_stickers(
     :param file_to_write: Name for the final file
     """
     stickers = process_paths(files_list)
-    writer = sticker_stacker(sticker_list(stickers))
+    writer = sticker_stacker(sticker_list(stickers), **kwargs)
 
     with open(file_to_write, 'wb') as fp:
         writer.write(fp)
@@ -179,12 +203,33 @@ def directory(value: str, result_dict: dict) -> dict:
     return result_dict
 
 
+def grid_parameters(attr_key: str, value: str, result_dict: dict) -> dict:
+    try:
+        result_dict[attr_key] = int(value)
+    except ValueError:
+        raise UnporcessableArgumentsError([(value, 'Should be an integer'), ])
+
+    return result_dict
+
+
+set_stickers_in_width = partial(grid_parameters, attr_key='stickers_in_width')
+set_stickers_in_height = partial(grid_parameters, attr_key='stickers_in_height')
+
+
+def set_paper_format(value: str, result_dict: dict) -> dict:
+    result_dict['paper_format'] = value
+    return result_dict
+
+
 def parse_arguments() -> dict[str, str]:
     """
     Parse command line arguments.
 
     -f - optional. Name of the final file to save
     -d - optional. Directory from which all pdf files will be used as input files
+    -w - optional. How many stickers should be placed across the page
+    -h - optional. How many stickers should be placed down the page
+    -s - optional. Paper format to use (e.g. A4, A3 etc.)
 
     :return: Dict of kwargs to main function
     """
@@ -193,6 +238,9 @@ def parse_arguments() -> dict[str, str]:
     implemented_options = {
         '-f': file_name,
         '-d': directory,
+        '-w': set_stickers_in_width,
+        '-h': set_stickers_in_height,
+        '-s': set_paper_format,
     }
 
     # Get list of options with their values and list of files to use
@@ -206,11 +254,15 @@ def parse_arguments() -> dict[str, str]:
 
     # For all parsed options call corresponding function with its value and result dict to modify
     for option, value in options:
-        implemented_options[option](value, result)
+        implemented_options[option](value=value, result_dict=result)
 
     return result
 
 
 if __name__ == '__main__':
     # print(parse_arguments())
-    compose_stickers(**parse_arguments())
+    try:
+        compose_stickers(**parse_arguments())
+    except UnporcessableArgumentsError as uae:
+        print(uae)
+        sys.exit(1)
