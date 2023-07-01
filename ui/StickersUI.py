3f59dec450081dc11d7267e3416ec054de75f914
Roma <you@example.com>
2023-07-01 19:11:21 +0200

    Update UI to specify stickers layout and paper size.

--- ui/StickersUI.py
+++ ui/StickersUI.py
@@ -3,9 +3,13 @@ from typing import Iterable, Callable
 from pathlib import Path
 
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import ttk, filedialog, messagebox
+from pypdf import PaperSize
 
-from app.__main__ import compose_stickers
+from app.__main__ import compose_stickers, UnporcessableArgumentsError
+
+
+PAPER_SIZES = [s for s in dir(PaperSize) if not s.startswith('__')]
 
 
 def browse_files() -> Iterable[Path]:
@@ -25,21 +29,57 @@ def browse_directory() -> Iterable[Path]:
 class StickersUI:
     def __init__(self, browse_functions: list[Callable[[], Iterable[Path]]]):
         self.root = tk.Tk()
-        self.root.title = "NP stickers"
+        self.root.title('Sticker stacker')
+        self.root.minsize(width=500, height=300)
+        self.root.eval('tk::PlaceWindow . center')
 
         # State variables
+        self.paper_size = tk.StringVar()
+        self.stickers_in_width = tk.IntVar()
+        self.stickers_in_height = tk.IntVar()
         self._browse_mode = tk.IntVar()
         self._file_list = []
         self.browse_functions = browse_functions
 
-        # Frames:
-        self.frm_radios = tk.Frame(master=self.root, width=500, pady=5)
-        self.frm_buttons = tk.Frame(master=self.root, width=500, padx=10, pady=5)
-        self.frm_file_list = tk.Frame(master=self.root, width=500, height=100, padx=10, pady=5, bd=1, relief=tk.SUNKEN)
+        self.stickers_in_width.set(2)
+        self.stickers_in_height.set(3)
 
+        # Frames:
+        self.frm_radios = tk.Frame(master=self.root, pady=5)
+        self.frm_buttons = tk.Frame(master=self.root, padx=10, pady=5)
+        self.frm_file_list = tk.Frame(master=self.root, height=100, padx=10, pady=5, bd=1, relief=tk.SUNKEN)
+
+        self.frm_layout = tk.Frame(master=self.root, padx=10, pady=10)
+        self.frm_grid = tk.Frame(master=self.frm_layout)
+        self.frm_paper_size = tk.Frame(master=self.frm_layout)
+
+        self.frm_layout.pack(fill=tk.X)
         self.frm_radios.pack(fill=tk.X)
         self.frm_buttons.pack(fill=tk.X)
         self.frm_file_list.pack(fill=tk.BOTH, expand=True)
+
+        self.frm_grid.pack(side=tk.LEFT)
+        self.frm_paper_size.pack(side=tk.RIGHT, fill=tk.BOTH, pady=2)
+
+        # Layout parameters:
+        self.cbx_paper_size = ttk.Combobox(master=self.frm_paper_size,
+                                           values=PAPER_SIZES,
+                                           width=5,
+                                           textvariable=self.paper_size,)
+        self.cbx_paper_size.current(PAPER_SIZES.index('A4'))
+
+        self.sbx_stickers_in_width = tk.Spinbox(master=self.frm_grid, from_=1, to=40, width=2,
+                                                textvariable=self.stickers_in_width)
+        self.sbx_stickers_in_heigth = tk.Spinbox(master=self.frm_grid, from_=1, to=40, width=2,
+                                                 textvariable=self.stickers_in_height)
+
+        self.sbx_stickers_in_width.grid(column=0, row=0)
+        tk.Label(master=self.frm_grid, text='stickers across the page').grid(column=1, row=0, sticky='w', padx=3)
+        self.sbx_stickers_in_heigth.grid(column=0, row=1)
+        tk.Label(master=self.frm_grid, text='stickers down the page').grid(column=1, row=1, sticky='w', padx=3)
+
+        tk.Label(master=self.frm_paper_size, text='Pape size:').grid(column=0, row=0, padx=3)
+        self.cbx_paper_size.grid(column=1, row=0)
 
         # Radios:
         self.rbtn1 = tk.Radiobutton(master=self.frm_radios,
@@ -99,4 +139,12 @@ class StickersUI:
             filetypes=[('PDF', ('*.pdf', '*.PDF')), ]
         )
         if file_to_save:
-            compose_stickers(self.file_list, file_to_save)
+            kwargs = {
+                'stickers_in_width': self.stickers_in_width.get(),
+                'stickers_in_height': self.stickers_in_height.get(),
+                'paper_format': self.paper_size.get(),
+            }
+            try:
+                compose_stickers(self.file_list, file_to_save, **kwargs)
+            except UnporcessableArgumentsError as e:
+                messagebox.showerror(message=str(e))
