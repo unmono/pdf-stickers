5feaafd67745f4202f6a654ded4f2c7b6ae84657
unmono <unmono.dev@gmail.com>
2024-06-08 00:14:44 +0200

    Add install.sh script. Modify run.sh. run.sh now runs venv, can be executed from other dirs install.sh checks interpreters, sets up environment, install dependencies

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+APP_DIR=$(cd "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+PY="python3"
+
+activate_venv() {
+  . "$APP_DIR/.venv/bin/activate"
+}
+exit_prompt() {
+  printf "%s. Press any key to exit...\n" "$1"
+  read -n 1 -s -r -p ""
+  exit "$2"
+}
+
+while getopts "p:" opt; do
+  case ${opt} in
+    p)
+      export PY="${OPTARG}"
+      ;;
+    ?)
+      exit_prompt "Invalid argument" 1
+      ;;
+  esac
+done
+if ! $PY -c "import tkinter"; then
+  exit_prompt "Tkinter required but not installed" 1
+fi
+if ! [ -d "$APP_DIR/.venv" ]; then
+  if ! ($PY -m venv "$APP_DIR/.venv" & pidV=$! && wait $pidV); then
+   exit_prompt "Failed to setup virtual environment" 1
+  fi
+fi
+if ! (activate_venv && pip install -r "$APP_DIR/requirements.txt"); then
+  exit_prompt "Failed to install requirements" 1
+fi
+exit_prompt "Finished" 0
--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
-source .venv/bin/activate
-python3 -m ui
+#!/bin/bash
+APP_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+activate() {
+  . "$APP_DIR/.venv/bin/activate"
+}
+activate
+cd "$APP_DIR" || exit 1
+setsid -f python3 -m ui
+exit 0
